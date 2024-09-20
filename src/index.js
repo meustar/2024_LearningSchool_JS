07646854fbe90b5b5194f3e8cd872be90191f363
meustar <meustar829@gmail.com>
2024-09-20 22:48:10 +0900

    sec03_1. 자바스크립트 환경 설정

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-let name = "KGB";
-let age = "32";
-let promise =
-  "늦게 시작한 만큼.. 빨리 따라가지는 못해도 천천히 끝까지 완독 하겠습니다.";
-
-console.log(name);
-console.log(age);
-console.log(promise);
+console.log("start VSCode");
